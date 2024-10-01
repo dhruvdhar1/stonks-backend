0ef680a0e9c74eb2d4a38bc16f4bdc7d5949bd11
Dhruv Dhar <dhruvdhar1@gmail.com>
2024-10-01 15:04:47 -0400

    updated precision

--- src/utils/historicalDataDeserializer.ts
+++ src/utils/historicalDataDeserializer.ts
@@ -2,7 +2,7 @@ export const historicalDataDeserializer = (data:any[]=[]) => {
     const res = data.map((quote: any) => {
         return {
             date: quote.date,
-            cost: quote.close
+            cost: quote.close?.toFixed(2)
         }
     })
     return res
